7368ac2fac522378bc052ab47a99d95e53c9feb2
catastrofe333 <mcatalinasanchezs@gmail.com>
2025-10-14 19:09:46 -0500

    Arreglar vagones

--- src/main/java/modelo/VagonCarga.java
+++ src/main/java/modelo/VagonCarga.java
@@ -1,24 +1,13 @@
 package modelo;
 
 public class VagonCarga extends Vagon {
-    private ListaEnlazada<Equipaje> equipajes;
 
     //Constructor
     public VagonCarga(String idVagon){
         super(idVagon);
-        this.equipajes = new ListaEnlazada<>();
-    }
-
-    //Getters
-    public ListaEnlazada<Equipaje> getEquipajes() {
-        return equipajes;
     }
 
     //Registrar un equipaje
-    public boolean agregarEquipaje(Equipaje equipaje){
-        equipajes.agregar(equipaje);
-        return true;
-    }
 
     //Mostrar informacion
     @Override
